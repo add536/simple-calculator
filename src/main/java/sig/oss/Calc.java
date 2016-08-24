80c91b6caed1ca1aba921eac67f5dd3df57aa2ff
Oh, Se Yoon <seyoonoh1@gmail.com>
2016-08-24 15:07:36 +0900

    clear error

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -5,7 +5,7 @@ public class Calc {
 	public int sub(int x, int y) {//subtraction methods
 		return x-y;
 	}
-	public int sub(int x, int y) {//multiply methods readme1 2
+	public int mul(int x, int y) {//multiply methods readme1 2
 		return x*y;
 	}
 
